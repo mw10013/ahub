c7726052108daae398ac93ab01ded894e7e8516b
Michael Wu <mw10013@gmail.com>
2022-03-10 17:13:56 -0500

    dump

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,46 @@ use clap::{Parser, Subcommand};
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
 struct Cli {
-    // #[clap(subcommand)]
-// command: Commands,
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Dump database
+    // #[clap(arg_required_else_help = true)]
+    Dump {
+        #[clap(subcommand)]
+        command: DumpCommands,
+    },
+    /// Mock access
+    #[clap(arg_required_else_help = true)]
+    Mock {
+        /// The remote to target
+        remote: String,
+    },
+}
+
+#[derive(Subcommand, Debug)]
+enum DumpCommands {
+    /// Dump events
+    // #[clap(arg_required_else_help = true)]
+    Events {
+        /// Number of events to take
+        #[clap(short, long, parse(try_from_str), default_value_t = 10)]
+        take: usize,
+    },
 }
 
 fn main() {
-    let _args = Cli::parse();
+    let args = Cli::parse();
+
+    match &args.command {
+        Commands::Dump { command } => {
+            println!("Dumping {:?}", command);
+        }
+        Commands::Mock { remote } => {
+            println!("Pushing to {}", remote);
+        }
+    }
 }
