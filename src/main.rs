cc4b6adbd8421d828a3bfcae890c1e7811cb8430
Michael Wu <mw10013@gmail.com>
2022-03-10 14:36:33 -0500

    added clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Cli {
+    // #[clap(subcommand)]
+// command: Commands,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _args = Cli::parse();
 }
