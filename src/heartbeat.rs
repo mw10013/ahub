d4517bd97ac6b22035cb8585d060a6ba0e0f3a26
Michael Wu <mw10013@gmail.com>
2022-03-17 16:58:42 -0400

    json

--- src/heartbeat.rs
+++ src/heartbeat.rs
@@ -15,22 +15,21 @@ struct HeartbeatRequestData {
 #[derive(Serialize, Deserialize)]
 struct HeartbeatAccessHubRequestData {
     id: i64,
+    cloudLastAccessEventAt: Option<String>,
+    accessEvents: Vec<HeartbeatAccessEventRequestData>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct HeartbeatAccessEventRequestData {
+    at: String,
+    access: String,
+    code: String,
+    accessUserId: Option<i64>,
+    accessPointId: i64,
 }
 
 pub async fn heartbeat(host: String) -> anyhow::Result<()> {
     /*
-    let mut map = HashMap::new();
-    map.insert("lang", "rust");
-    map.insert("body", "json");
-
-    let client = reqwest::Client::new();
-    let res = client
-        .post("http://httpbin.org/post")
-        .json(&map)
-        .send()
-        .await?;
-    println!("{:#?}", res);
-
     let ip = reqwest::get("http://httpbin.org/ip")
         .await?
         .json::<Ip>()
@@ -39,7 +38,11 @@ pub async fn heartbeat(host: String) -> anyhow::Result<()> {
         */
 
     let heartbeat_request_data = HeartbeatRequestData {
-        accessHub: HeartbeatAccessHubRequestData { id: 1 },
+        accessHub: HeartbeatAccessHubRequestData {
+            id: 1,
+            cloudLastAccessEventAt: None,
+            accessEvents: vec![],
+        },
     };
     let client = reqwest::Client::new();
     let res = client
@@ -48,6 +51,16 @@ pub async fn heartbeat(host: String) -> anyhow::Result<()> {
         .send()
         .await?;
     println!("{:#?}", res);
+
+    if res.status().is_success() {
+        // let json = res.json().await?;
+        // println!("json: {:#?}", json);
+        let text = res.text().await?;
+        println!("json: {}", text);
+    } else {
+        let text = res.text().await?;
+        println!("error: {}", text);
+    }
 
     Ok(())
 }
