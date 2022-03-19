bf46b9fa0f12d66da01009890f040b2844e05e28
Michael Wu <mw10013@gmail.com>
2022-03-19 16:36:55 -0400

    heartbeat

--- src/heartbeat.rs
+++ src/heartbeat.rs
@@ -4,28 +4,24 @@ use crate::domain::Hub;
 // use anyhow::Context;
 use sqlx::sqlite::SqlitePool;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
 struct RequestData {
-    accessHub: AccessHubRequestData,
+    access_hub: AccessHubRequestData,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
 struct AccessHubRequestData {
     id: i64,
-    cloudLastAccessEventAt: Option<String>,
-    accessEvents: Vec<AccessEventRequestData>,
+    cloud_last_access_event_at: Option<chrono::NaiveDateTime>,
+    access_events: Vec<AccessEventRequestData>,
 }
 
-// toJSON(): '2022-03-18T18:00:53.188Z'
-// sqlite: 2022-03-15 22:17:57
-// NaiveDateTime: 2022-03-15T22:17:57
-// format("%Y-%m-%dT%H:%M:%S.000Z")
-
-#[derive(Debug, Serialize, Deserialize, sqlx::FromRow)]
+#[derive(Debug, Serialize, sqlx::FromRow)]
 #[sqlx(rename_all = "camelCase")]
 #[serde(rename_all = "camelCase")]
 struct AccessEventRequestData {
-    // at: String,
     #[serde(with = "json_date_format")]
     at: chrono::NaiveDateTime,
     access: String,
@@ -34,19 +30,19 @@ struct AccessEventRequestData {
     access_point_id: i64,
 }
 
+// https://serde.rs/custom-date-format.html
 mod json_date_format {
     use chrono::NaiveDateTime;
     use serde::{self, Deserialize, Deserializer, Serializer};
 
-    // const FORMAT: &str = "%Y-%m-%d %H:%M:%S";
-    const FORMAT: &str = "%Y-%m-%dT%H:%M:%S.000Z";
+    const FORMAT: &str = "%Y-%m-%dT%H:%M:%S.000Z"; // JS Date.toJSON()
 
     pub fn serialize<S>(dt: &NaiveDateTime, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
     {
         let s = format!("{}", dt.format(FORMAT));
-        println!("serialize: {} {}", dt, &s);
+        // println!("serialize: {} {}", dt, &s);
         serializer.serialize_str(&s)
     }
 
@@ -55,17 +51,13 @@ mod json_date_format {
         D: Deserializer<'de>,
     {
         let s = String::deserialize(deserializer)?;
-        // Utc.datetime_from_str(&s, FORMAT).map_err(serde::de::Error::custom)
-        println!("deserialize: {}", &s);
+        // println!("deserialize: {}", &s);
         NaiveDateTime::parse_from_str(&s, FORMAT).map_err(serde::de::Error::custom)
     }
 }
 
 pub async fn heartbeat(host: String, pool: &SqlitePool) -> anyhow::Result<()> {
-    // select strftime('%Y-%m-%dT%H:%M:%fZ', 'now');
-    // SELECT strftime('%s'); -- %s		seconds since 1970-01-01
-    // SELECT strftime('%f'); -- %s		seconds since 1970-01-01
-    // SELECT sqlite_version();
+    // DATETIME(current_timestamp, '-5 minutes')
     /*
     let ip = reqwest::get("http://httpbin.org/ip")
         .await?
@@ -79,35 +71,33 @@ pub async fn heartbeat(host: String, pool: &SqlitePool) -> anyhow::Result<()> {
         .await?;
     println!("{:#?}", hub);
 
-    let events: Vec<AccessEventRequestData> = sqlx::query_as(
-        "select at, access, code, accessUserId, accessPointId from AccessEvent order by at desc",
-    )
-    .fetch_all(pool)
-    .await?;
+    // let events: Vec<AccessEventRequestData> = sqlx::query_as(
+    //     "select at, access, code, accessUserId, accessPointId from AccessEvent order by at desc",
+    // )
+    // .fetch_all(pool)
+    // .await?;
 
+    // select * from AccessEvent where at > '2022-03-15 23:00:00' and at < DATETIME(CURRENT_TIMESTAMP, '-5 seconds') order by at desc;
+    let events: Vec<AccessEventRequestData> = match hub.cloud_last_access_event_at {
+        Some(cloud_last_access_event_at) => {
+            // Leave margin to prevent race condition.
+            sqlx::query_as(
+                "select at, access, code, accessUserId, accessPointId from AccessEvent 
+                where at > ? and at < DATETIME(CURRENT_TIMESTAMP, '-5 seconds') order by at desc",
+            )
+            .bind(cloud_last_access_event_at)
+            .fetch_all(pool)
+            .await?
+        }
+        None => vec![],
+    };
     dbg!(&events);
 
-    let e = &events[0];
-    println!("e: {:#?}", e);
-    // println!(
-    //     "at: {} {} {}",
-    //     e.at,
-    //     e.at.format("%Y-%m-%d %H:%M:%S"),
-    //     e.at.format("%Y-%m-%dT%H:%M:%S.000Z")
-    // );
-
-    let json = serde_json::to_string_pretty(&e).unwrap();
-    println!("json: {}", json);
-    let data: AccessEventRequestData = serde_json::from_str(&json).unwrap();
-    println!("data: {:#?}", data);
-    println!("data.at: {}", data.at);
-
-
     let request_data = RequestData {
-        accessHub: AccessHubRequestData {
-            id: 1,
-            cloudLastAccessEventAt: None,
-            accessEvents: vec![],
+        access_hub: AccessHubRequestData {
+            id: hub.id,
+            cloud_last_access_event_at: hub.cloud_last_access_event_at,
+            access_events: events,
         },
     };
     let client = reqwest::Client::new();
@@ -120,7 +110,7 @@ pub async fn heartbeat(host: String, pool: &SqlitePool) -> anyhow::Result<()> {
 
     if res.status().is_success() {
         let json: serde_json::Value = res.json().await?;
-        // println!("json: {:#?}", json);
+        println!("json: {:#?}", json);
     } else {
         let text = res.text().await?;
         println!("error: {}", text);
