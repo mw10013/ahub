ba1f463050e18dda8d90cadb0b7081464c3e6f00
Michael Wu <mw10013@gmail.com>
2022-03-30 10:00:58 -0400

    removed if exists

--- migrations/20220325160509_schema.sql
+++ migrations/20220325160509_schema.sql
@@ -1,19 +1,19 @@
-create table if not exists AccessHub (
+create table AccessHub (
     id integer not null primary key,
     cloud_last_access_event_at datetime
 );
-create table if not exists AccessPoint (
+create table AccessPoint (
     id integer not null primary key,
     position integer not null
 );
-create table if not exists AccessUser (
+create table AccessUser (
     id integer not null primary key,
     name text not null default '',
     code text not null,
     activate_code_at datetime,
     expire_code_at datetime
 );
-create table if not exists AccessEvent (
+create table AccessEvent (
     id integer not null primary key,
     at datetime not null,
     access text not null,
@@ -22,14 +22,14 @@ create table if not exists AccessEvent (
     access_point_id integer not null,
     constraint AccessEvent_access_point_id_fkey foreign key (access_point_id) references AccessPoint (id) on delete restrict on update cascade
 );
-create table if not exists AccessPointToAccessUser (
+create table AccessPointToAccessUser (
     access_point_id integer not null,
     access_user_id integer not null,
     foreign key (access_point_id) references AccessPoint (id) on delete cascade on update cascade,
     foreign key (access_user_id) references AccessUser (id) on delete cascade on update cascade
 );
 
-create view if not exists ActiveCode as 
+create view ActiveCode as 
 select access_point_id, position, code, access_user_id, activate_code_at, expire_code_at 
 from AccessUser u join AccessPointToAccessUser p2u on u.id = p2u.access_user_id 
   join AccessPoint p on p2u.access_point_id = p.id 
